096ffa5ef7282998b6d013b8f823023f43c17d7c
kangseoyeon05 <seoyeo1360@naver.com>
2025-04-10 23:01:48 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,11 @@
+name = input("Input his/her name: ")
+
+msg1 = "Hello " + name + ","
+msg2 = "Welcome to Seoul."
+
+line = "----------------------------------------"
+
+print(line)
+print(msg1)
+print(msg2)
+print(line)
